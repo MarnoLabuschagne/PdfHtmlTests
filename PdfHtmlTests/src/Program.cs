7b3310823a5a0f091f097e1208e24c8bc7cd79e8
Labuschagne <Marno.Labuschagne@gnb.ca>
2023-02-06 13:55:57 -0400

    Crude user interface via command line

--- PdfHtmlTests/src/Program.cs
+++ PdfHtmlTests/src/Program.cs
@@ -15,8 +15,21 @@ namespace PdfHtmlTests.src
                 "C:\\Users\\malabuschagne\\source\\repos\\PdfHtmlTests\\PdfHtmlTests\\docs\\2column.pdf",
                 "C:\\Users\\malabuschagne\\source\\repos\\PdfHtmlTests\\PdfHtmlTests\\docs\\basic.pdf"};
 
-            int choice = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("1: Read all text from pdf\n" +
+                "11: print hello from template\n" +
+                "12: print template\n" +
+                "13: save Hello template to stream\n" +
+                "14: save Hello template as helloTemplate.html\n" +
+                "15: load helloTemplate.html\n" +
+                "16: save template as basicTestTemplate.html\n" +
+                "17: load template basicTestTemplate.html\n" +
+                //"18: get string from a razor page\n" +
+                "21: save a Hello World pdf\n" +
+                "22: convert 'Hello' html to pdf\n" +
+                "23: convert longer template html to pfg\n"
+                );
 
+            int choice = Int32.Parse(Console.ReadLine());            
             switch (choice)
             {
                 case 1:
@@ -39,10 +52,10 @@ namespace PdfHtmlTests.src
                     razorTemplate.loadHelloTemplate("15Marno", "helloTemplate.html");
                     break;
                 case 16:
-                    razorTemplate.saveLongTemplate("basicTestTemplate2.html", "16Marno", 5);
+                    razorTemplate.saveLongTemplate("basicTestTemplate.html", "16Marno", 5);
                     break;
                 case 17:
-                    razorTemplate.loadLongTemplate("basicTestTemplate2.html", "17Marno", 5);
+                    razorTemplate.loadLongTemplate("basicTestTemplate.html", "17Marno", 5);
                     break;
                 case 18:
                     razorTemplate.stringFromRazorPage("C:\\Users\\malabuschagne\\source\\repos\\PdfHtmlTests\\PdfHtmlTests\\PagesX\\SharedX\\Index.cshtml");
